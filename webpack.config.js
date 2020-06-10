6275c75ff4c9d4d3697d8bf272201c8b3822eb19
Marco Arellano <mgz.arellano@gmail.com>
2020-06-10 11:26:37 -0500

    Correction of the yaml file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require ('copy-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -14,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: "echo \"No test specified\""
+        test: /\.js?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
